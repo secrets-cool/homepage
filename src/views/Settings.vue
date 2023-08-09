8e777e81594e058cd6a5bfea3c6bf6f39bf8e5b5
talrasha007 <talrasha007@gmail.om>
2023-08-09 20:58:42 +0800

    ui settings.

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,4 +1,20 @@
+<script setup>
+import { ref, watch } from 'vue'
+import { NCard, NForm, NGrid, NFormItemGi, NSwitch } from 'naive-ui'
+
+import { theme } from '../settings'
+const isDark = ref(theme.value === 'dark')
+watch(isDark, (value) => { theme.value = value ? 'dark' : 'light' })
+</script>
 
 <template>
-  SSS
+  <n-card title="UI">
+    <n-form label-placement="left">
+      <n-grid cols="4">
+        <n-form-item-gi label="Dark theme">
+          <n-switch v-model:value="isDark" />
+        </n-form-item-gi>
+      </n-grid>
+    </n-form>
+  </n-card>
 </template>
