3e9bf577076dad3476e2cc693e8c753a679ec150
talrasha007 <talrasha007@gmail.om>
2023-08-10 09:57:52 +0800

    use cdn.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { Plugin as importToCDN, autoComplete } from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    importToCDN({
+      prodUrl: 'https://npm.elemecdn.com/{name}@{version}/{path}',
+      modules: [
+        autoComplete('vue'),
+        {
+          name: 'naive-ui',
+          var: 'naive',
+          path: 'dist/index.prod.js'
+        }
+      ]
+    })
+  ],
   server: {
     port: 15173,
   }
